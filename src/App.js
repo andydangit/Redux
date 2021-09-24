f11f24ab9c8130e811db4597ad855f8016c05cb8
AndyDangIt <andy.dang.it@gmail.com>
2021-09-24 15:11:09 -0500

    add useSelector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useSelector } from 'react-redux'
 
 import './App.css';
 
 function App() {
+
+const state = useSelector((state) => state)
+
+console.log(state);
+
   return (
     <div className="App">
 Redux 
