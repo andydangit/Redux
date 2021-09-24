8099bc1418e13d33f92210afaf7f402bbe6e24f0
AndyDangIt <andy.dang.it@gmail.com>
2021-09-24 15:31:47 -0500

    files that hold all the action

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 function App() {
 
-const state = useSelector((state) => state)
+const account = useSelector((state) => state.account)
 
-console.log(state);
+console.log(account);
 
   return (
     <div className="App">
