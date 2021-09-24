3aba06e53b32b3ac0f2f9b878092f5a032c67f34
AndyDangIt <andy.dang.it@gmail.com>
2021-09-24 14:41:02 -0500

    add store as a prop

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from " react-redux";
+import { store } from '../src/state/store'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
