931b8b51585a6637cc1be67e3824ec1482ed2e18
pandoragear <andy.dang.it@gmail.com>
2021-09-24 23:31:46 -0500

    add dispatch withdrawMoney

--- src/state/action-creators/index.js
+++ src/state/action-creators/index.js
@@ -6,3 +6,12 @@ export const depositMoney = (amount) => {
         })
     }
 }
+
+export const withdrawMoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: "withdraw",
+            payload: amount
+        })
+    }
+}
