cb672dfb3fb928dbffe09c63062b1bbf2aa95134
pandoragear <andy.dang.it@gmail.com>
2021-09-24 23:42:56 -0500

    add actionCreator and fix bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useSelector, useDispatch } from 'react-redux'
 import {bindActionCreators } from 'redux'
-import {} from './state/'
 import { actionCreators } from './state/index'
 
 function App() {
 
 const account = useSelector((state) => state.account);
 
-const dispatch = useDispatch()
+const dispatch = useDispatch();
+
+const AC = bindActionCreators(actionCreators, dispatch)
+
+console.log(AC);
 
 console.log(account);
 
