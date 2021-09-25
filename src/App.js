27f054a5ba9f917f1a43d060bd4f533c6a009ff4
pandoragear <andy.dang.it@gmail.com>
2021-09-25 00:02:14 -0500

    remove log(AC / account) added bindActioncreators

--- src/App.js
+++ src/App.js
@@ -8,15 +8,18 @@ const account = useSelector((state) => state.account);
 
 const dispatch = useDispatch();
 
-const AC = bindActionCreators(actionCreators, dispatch)
+const { depositMoney, withdrawMoney  } = bindActionCreators(actionCreators, dispatch)
 
-console.log(AC);
 
-console.log(account);
+
+
 
   return (
     <div className="App">
-Redux 
+<h1> {account} </h1>
+<button onClick={() => depositMoney(1000)}> Deposit </button> 
+<button onClick={() => withdrawMoney(1000)}> withdraw </button> 
+
     </div>
   );
 }
